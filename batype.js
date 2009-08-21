b8d3b614569197349b2c2ffb612b598af42b392e
Florian Herlings <florian.herlings@googlemail.com>
2009-08-21 16:35:57 +0200

    Added GPL license to the javascript file

--- batype.js
+++ batype.js
@@ -1,3 +1,26 @@
+/*
+* ByType is a small game based on canvas that helps you to learn
+* typing faster. It is more a proof of concept.
+* 
+* Copyright (C) 2009 Florian Herlings (florianherlings.de)
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+* Version:    0.1
+* Date:       21.08.2009
+*/
+
 
 
 var Config = {
